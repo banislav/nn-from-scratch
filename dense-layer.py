1a278af421dac2bf47eec098d56e641a2c351644
banikrukov <danyalapin@yandex.ru>
2023-05-30 18:25:39 +0500

    [MODIFY] implement dense layer class with methods

--- dense-layer.py
+++ dense-layer.py
@@ -4,14 +4,23 @@ from typing import List
 
 
 class DenseLayer(Layer):
-    def __init__(self, n_inputs, n_neurons, learning_rate=0.01):
+    def __init__(self, n_inputs: int, n_outputs: int, learning_rate: int = 0.01):
         super().__init__()
         self.learning_rate = learning_rate
         self.n_inputs = n_inputs
-        self.n_neurons = n_neurons
+        self.n_outputs = n_outputs
+        self.weights = np.random.normal(loc=0.0,
+                                        scale=np.sqrt(2 / (n_inputs + n_outputs)),
+                                        size=(n_inputs, n_outputs))
+        self.biases = np.zeros_like(n_outputs)
 
-    def forward(self, input_data: List):
-        pass
+    def forward(self, X: List[List, List]) -> List[List, List]:
+        return np.dot(X, self.weights) + self.biases
 
-    def backward(self, grads, input_data):
-        pass
+    def backward(self, X: np.array, grads: np.array):
+        loss_grads = np.dot(self.weights, grads)
+
+        self.weights -= np.dot(X, grads) * self.learning_rate
+        self.biases -= self.learning_rate * grads.mean(axis=0)*X.shape[0]
+
+        return loss_grads
