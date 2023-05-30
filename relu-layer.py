159aec7a834d441f4ed3c545f44898330b99be1d
banikrukov <danyalapin@yandex.ru>
2023-05-30 18:36:03 +0500

    [MODIFY] implement relu output layer class

---
+++ relu-layer.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from layer import Layer
+
+
+class ReluLayer(Layer):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input_data: np.ndarray):
+        return np.max(0, input_data)
+
+    def backward(self, grads: np.ndarray, X: np.ndarray):
+        return grads * (X > 0)
